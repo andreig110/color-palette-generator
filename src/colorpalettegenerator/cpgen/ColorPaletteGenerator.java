c4d326af5ff68d78b0c17941fefd9e11e4e82902
Andrei Guga <icandreig@gmail.com>
2019-11-21 16:00:32 +0200

    Add code to generate colors with different brightness

--- src/colorpalettegenerator/cpgen/ColorPaletteGenerator.java
+++ src/colorpalettegenerator/cpgen/ColorPaletteGenerator.java
@@ -8,17 +8,19 @@ public class ColorPaletteGenerator {
 
     public static void generate(int numOfHues, int numOfSaturations, int numOfBrightnesses, Canvas canvas, int paletteColorSize) {
         final double hueStep = 360d / numOfHues;
-        final double saturationStep = 0.8d / numOfSaturations;
-        final double brightnessStep = 1d / numOfBrightnesses;
+        final double saturationStep = 1d / (numOfSaturations + 1);
+        final double brightnessStep = 1d / (numOfBrightnesses + 1);
 
         canvas.setWidth(numOfHues * paletteColorSize);
-        canvas.setHeight(numOfSaturations * paletteColorSize);
+        canvas.setHeight(numOfSaturations * numOfBrightnesses * paletteColorSize);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         for (int i = 0; i < numOfHues; ++i) {
             for (int j = 0; j < numOfSaturations; ++j) {
-                gc.setFill(Color.hsb(i * hueStep, (j + 1) * saturationStep, 0.8));
-                gc.fillRect(i * paletteColorSize, j * paletteColorSize, paletteColorSize, paletteColorSize);
+                for (int k = 0; k  < numOfBrightnesses; ++k) {
+                    gc.setFill(Color.hsb(i * hueStep, (j + 1) * saturationStep, (k + 1) * brightnessStep));
+                    gc.fillRect(i * paletteColorSize, (j*numOfBrightnesses + k) * paletteColorSize, paletteColorSize, paletteColorSize);
+                }
             }
         }
     }
